e7e7a553ea5aa0e0432fdfe6e5888e9c3375bee8
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2021-01-30 21:55:50 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,10 @@ import { ROUTES } from './app.routes';
 
 //pipes
 import { NoimagePipe } from './pipes/noimage.pipe';
+
 import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
 import { LoadingComponent } from './components/shared/loading/loading.component';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
 
 @NgModule({
   declarations: [
@@ -25,6 +27,7 @@ import { LoadingComponent } from './components/shared/loading/loading.component'
     SearchComponent,
     ArtistaComponent,
     NavbarComponent,
+    DomseguroPipe,
     NoimagePipe,
     TarjetasComponent,
     LoadingComponent,
