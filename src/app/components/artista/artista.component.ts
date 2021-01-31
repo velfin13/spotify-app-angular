757c9b260f688cffd950d75e1b09dfb0a827d6e7
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2021-01-30 21:04:43 -0500

    agregado audio en pagina artista

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-artista',
@@ -7,9 +8,33 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./artista.component.css'],
 })
 export class ArtistaComponent implements OnInit {
-  constructor(private router: ActivatedRoute) {
+  artista: any = {};
+  loading: boolean;
+  topTracks: any[] = [];
+
+  constructor(private router: ActivatedRoute, private spotify: SpotifyService) {
+    this.loading = true;
     this.router.params.subscribe((param) => {
-      console.log(param['id']);
+      this.obtenerArtista(param['id']);
+      this.obtenerTopTracks(param['id']);
+    });
+  }
+
+  obtenerArtista(id: string) {
+    this.loading = true;
+    this.spotify.getArtista(id).subscribe((artist) => {
+      this.artista = artist;
+      this.loading = false;
+    });
+  }
+
+  obtenerTopTracks(id: string) {
+    this.loading = true;
+    this.spotify.getTopTracks(id).subscribe((dattopTracks) => {
+      this.loading = false;
+      this.topTracks = dattopTracks;
+      console.log(this.topTracks);
+      
     });
   }
 
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -7,13 +7,12 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class SpotifyService {
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   getUrl(query: string) {
     const url = `https://api.spotify.com/v1/${query}`;
     const tokens =
-      'BQBJij3gPwUEbbGhVHqDA0EkiM8ZlITfJFIfIoLBwnELT4bKnMVkdkzNiY6Pw9urepP6nLfrkJrSNQPN0MyGZWT69r3QtXr7k-RezuQt5z_bF8NJLs8zCPfluO5CjVAkitcUJsEtaHMb4LPQdA55rm8bOqJOEmg';
+      'BQBJ7b3yDADs8423F2xx7S0XTmhgNk0l7yvz35ujsDq5kFOuUzCXnKyn8o2OwNMwNucoj8PTU2CQPALs0cASgS72_DFudGHqA8FkEP2Snba669VPW54poFllN1oCnx1r-bC8hefayIzROEX3MsXTugi47p97QHk';
     const headers = new HttpHeaders({
       Authorization: `Bearer ${tokens}`,
     });
@@ -33,5 +32,13 @@ export class SpotifyService {
     return this.getUrl(`search?q=${termino}&type=artist&limit=20`).pipe(
       map((data: any) => data.artists.items)
     );
+  }
+
+  getArtista(id: string) {
+    return this.getUrl(`artists/${id}`);
+  }
+
+  getTopTracks(id: string) {
+    return this.getUrl(`artists/${id}/top-tracks?market=es`).pipe(map((data:any)=>data.tracks))
   }
 }
